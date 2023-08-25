379ab50cbcb6ca06ced0c63d6c8cdb313a37b30b
daviddasilva <davidsilva90131@gmail.com>
2023-08-25 16:27:00 -0400

    Implement and integrate FastAPI backend api

--- app/app/databases/client-page.js
+++ app/app/databases/client-page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { xcodeDark, xcodeLight, defaultSettingsXcodeLight } from "@uiw/codemirror-theme-xcode";
 import { sql } from "@codemirror/lang-sql";
@@ -18,7 +18,15 @@ import {
   SimpleGrid,
   VStack,
   GridItem,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
 } from "@chakra-ui/react";
+import axios from "axios";
 import {
   TbArrowBack,
   TbArrowForward,
@@ -29,27 +37,113 @@ import PageHeader from "@/components/page-header";
 import { useSidebar } from "@/lib/sidebar";
 import DataTable from "@/components/data-table";
 import { TreeView } from "@/components/tree-view";
-import { columns, rows, tree } from "./mock";
+import { columns } from "./mock";
 import Pagination from "@/components/pagination";
 
 const EXTENSIONS = {
   sql: sql(),
 };
 
+const Types = ["CQL", "NL"];
+
 export default function DatabasesClientPage() {
   const borderColor = useColorModeValue("gray.50", "#333");
   const menu = useSidebar();
+
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [dbName, setDBName] = useState("");
+  const [connectedDBName, setConnectedDBName] = useState(null);
+  const [treeData, setTreeData] = useState([]);
+  const [queryType, setQueryType] = useState(Types[0]);
+  const [tableData, setTableData] = useState([]);
 
   const [currentPage, setCurrentPage] = useState(null);
   const [pageSize, setPageSize] = useState(null);
 
   const paginatedRows = useMemo(() => {
     if (currentPage && pageSize) {
-      return rows.slice((currentPage - 1) * pageSize, currentPage * pageSize);
+      return tableData.slice(
+        (currentPage - 1) * pageSize,
+        currentPage * pageSize
+      );
     } else {
       return [];
     }
-  }, [currentPage, pageSize]);
+  }, [currentPage, pageSize, tableData]);
+
+  const onConnectModalOpen = () => {
+    setIsOpenModal(true);
+  };
+
+  const onConnectModalClose = () => {
+    setIsOpenModal(false);
+  };
+
+  const onConnectDB = async () => {
+    try {
+      await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/databases`, {
+        database_name: dbName,
+      });
+      setConnectedDBName(dbName);
+      onConnectModalClose();
+    } catch (err) {
+      setConnectedDBName(null);
+    }
+  };
+
+  const fetchData = async (dbName) => {
+    try {
+      const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/databases/${dbName}/keyspaces`
+      );
+      const trees = await Promise.all(
+        res.data.map(async (keySpace) => {
+          const res = await axios.get(
+            `${process.env.NEXT_PUBLIC_API_URL}/databases/${dbName}/keyspaces/${keySpace}/tables`
+          );
+          return {
+            key: keySpace,
+            label: keySpace,
+            descendants: res.data.map((table) => ({
+              key: table,
+              label: table,
+            })),
+          };
+        })
+      );
+      setTreeData([
+        {
+          key: dbName,
+          label: dbName,
+          descendants: trees,
+        },
+      ]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    if (connectedDBName) {
+      fetchData(connectedDBName);
+    }
+  }, [connectedDBName]);
+
+  const onRunQuery = async () => {
+    try {
+      const res = await axios.post(
+        `${
+          process.env.NEXT_PUBLIC_API_URL
+        }/databases/${connectedDBName}/${queryType.toLowerCase()}`,
+        {
+          query: "SELECT * from TABLE",
+        }
+      );
+      setTableData(res.data.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <Stack flex={1} paddingX={4} paddingY={4} spacing={4}>
@@ -60,9 +154,13 @@ export default function DatabasesClientPage() {
       <SimpleGrid columns={4} flex={1} spacing={4}>
         <GridItem>
           <VStack h="100%" textAlign="left" spacing={2}>
-            <Input defaultValue="Search..." />
+            <Input placeholder="Search..." />
             <Box textAlign="right" w="100%">
-              <Button size="sm" leftIcon={<Icon as={TbPlus} />}>
+              <Button
+                size="sm"
+                leftIcon={<Icon as={TbPlus} />}
+                onClick={onConnectModalOpen}
+              >
                 Connect
               </Button>
             </Box>
@@ -75,7 +173,11 @@ export default function DatabasesClientPage() {
                 p: 2,
               }}
             >
-              <TreeView value={tree} isReadOnly={false} isDisabled={false} />
+              <TreeView
+                value={treeData}
+                isReadOnly={false}
+                isDisabled={false}
+              />
             </Box>
           </VStack>
         </GridItem>
@@ -90,13 +192,21 @@ export default function DatabasesClientPage() {
                   size="sm"
                   colorScheme="blue"
                   icon={<TbPlayerPlay />}
+                  onClick={onRunQuery}
                 />
                 <Text whiteSpace="nowrap" ml={4}>
                   Query Type:
                 </Text>
-                <Select variant="outline">
-                  <option value="CQL">CQL</option>
-                  <option value="NL">NL</option>
+                <Select
+                  variant="outline"
+                  value={queryType}
+                  onChange={(e) => setQueryType(e.target.value)}
+                >
+                  {Types.map((type, index) => (
+                    <option key={`query-type-${index}`} value={type}>
+                      {type}
+                    </option>
+                  ))}
                 </Select>
               </Flex>
             </HStack>
@@ -114,7 +224,7 @@ export default function DatabasesClientPage() {
             </Box>
             <Box>
               <Pagination
-                total={rows.length}
+                total={tableData.length}
                 onPageSizeChange={setPageSize}
                 onCurrentPageChange={setCurrentPage}
               />
@@ -122,6 +232,31 @@ export default function DatabasesClientPage() {
           </VStack>
         </GridItem>
       </SimpleGrid>
+      <Modal isOpen={isOpenModal} onClose={onConnectModalClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Connect Database</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <HStack spacing={2}>
+              <Text size="md">Database name</Text>
+              <Input
+                size="md"
+                value={dbName}
+                onChange={(e) => setDBName(e.target.value)}
+              />
+            </HStack>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onConnectModalClose}>
+              Close
+            </Button>
+            <Button variant="ghost" onClick={onConnectDB}>
+              Connect
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Stack>
   );
 }
